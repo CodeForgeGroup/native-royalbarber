c8059a6d0e140bdaeb4642901fc8994429ccedee
Gustavo Brito <ggustavobrito124@gmail.com>
2024-06-04 17:25:33 -0300

    Arquivos do corte, mesclagem de nagações

--- src/pages/Servicos/index.js
+++ src/pages/Servicos/index.js
@@ -3,22 +3,20 @@ import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, ScrollView, SafeAreaView, Image } from 'react-native';
 import { estilo } from './../estilo'
 
-const CustomButton = ({ onPress, title }) => (
-    <TouchableOpacity onPress={onPress} style={estilo.btnAgenda}>
-      <Text style={estilo.textAgenda}>{title}</Text>
-    </TouchableOpacity>
-  );
+const CustomButton = ({ onPress, title, buttonStyle, textStyle }) => (
+  <TouchableOpacity onPress={onPress} style={[estilo.botao, buttonStyle]}>
+    <Text style={[estilo.textoBotao, textStyle]}>{title}</Text>
+  </TouchableOpacity>
+);
+
 
 export default function Servicos({ navigation }){    
 return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }} style={{ backgroundColor:'white', }}>
     <SafeAreaView style={{ flex: 1, backgroundColor:'white' }}>
-      {/* TOPO */}
-      <View style={estilo.topo}>
-        <Image source={require('../../assets/logoLaranja.svg')} style={estilo.logo} />
-        <Text style={estilo.textOla}>Olá, <Text style={{color: 'orange'}}>'USUÁRIO'</Text><br/>Seja bem-vindo(a)</Text>
-      <CustomButton title="AGENDAR" onPress={() => navigation.navigate('Inicio')} ></CustomButton>
-      </View>
+      
+
+      <CustomButton title="CORTE MAQUINA" onPress={() => navigation.navigate('Corte na máquina')} />
     </SafeAreaView>
   </ScrollView>
     );
